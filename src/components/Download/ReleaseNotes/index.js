8901e00ea40115b33fd3df24f7df71d6b8714882
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-11-16 17:13:29 +0530

    Add releaseNotes component

---
+++ src/components/Download/ReleaseNotes/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import ReactMarkdown from 'react-markdown'
+
+
+const ReleaseNotes = ({ notes }) => (
+  <>
+      <h2>
+        <span role="img" aria-label="Notes">📝</span>
+        Release Notes
+      </h2>
+      <br />
+    {(notes.slice(0, 4).map(release => (
+      <div key={release.node_id}>
+        <ReactMarkdown>{release.body}</ReactMarkdown>
+        <br />
+      </div>
+    )))}
+  </>
+)
+
+export default ReleaseNotes;
