1377091c6006a1b3fb95a34b5637cdb8305b1158
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-05-17 16:54:17 +0530

    add images to landing page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,64 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import { Button } from '../Elements'
 import HomePerks from './HomePerks'
 
 import './index.scss'
 
-const Home = () => (
-  <div className="home">
-    <div className="home__content">
-      <div className="home__content__nav">
-        <div className="home__content__nav__header">
-          <h1 className="home__content__nav__header__brand">Invoicify</h1>
-          <Link to="/download">
-            <Button
-              secondary
-              className="home__content__nav__help-btn"
-            >
-              Why Invoicify?
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <div className="home__content__wrapper">
-        <div className="home__content__main">
-          <div className="home__content__main__text">
-            <h1 className="home__content__main__text__title">Extremely customizable</h1>
-            <h1 className="home__content__main__text__sub-title">& reliable Invoice software</h1>
-            <h1 className="home__content__main__text__sub-title--small">A complete invoice solution for businesses</h1>
-          </div>
-          <div className="home__content__main__download-section">
+const screenshots = [
+  'lock', 'personalize', 'preview',
+  'print', 'settings', 'update',
+]
+
+const Home = () => {
+  const [currentScreenshot, setCurrentScreenshot] = useState(0)
+
+  setTimeout(() => {
+    const newSS = currentScreenshot === screenshots.length - 1 ? 0 : currentScreenshot + 1
+    setCurrentScreenshot(newSS)
+  }, 3600)
+
+  return (
+    <div className="home">
+      <div className="home__content">
+        <div className="home__content__nav">
+          <div className="home__content__nav__header">
+            <h1 className="home__content__nav__header__brand">Invoicify</h1>
             <Link to="/download">
               <Button
                 secondary
-                className="home__content__main__download-section__dwnld-btn"
+                className="home__content__nav__help-btn"
               >
                 Download
               </Button>
             </Link>
           </div>
         </div>
+        <div className="home__content__wrapper">
+          <div className="home__content__main">
+            <TransitionGroup>
+              <CSSTransition
+                key={currentScreenshot}
+                appear
+                timeout={800}
+                classNames="scale-down"
+              >
+                <img
+                  className="home__content__main__img"
+                  alt={screenshots[currentScreenshot]}
+                  src={`img/ss/${screenshots[currentScreenshot]}.jpg`}
+                />
+              </CSSTransition>
+            </TransitionGroup>
+          </div>
 
-        <div className="home__content__perks">
-          <HomePerks />
+          <div className="home__content__perks">
+            <HomePerks />
+          </div>
         </div>
       </div>
+      <a
+        className="home__gh-link"
+        href="https://github.com/2AMDevs/invoicify-app"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img
+          alt="github icon"
+          className="home__gh-link__img"
+          src="https://camo.githubusercontent.com/351dcb42fb2be38ad786b8e29aa8b6b9dc9c2c8905aa451a1074f1ca5a76ce64/68747470733a2f2f696d6167652e666c617469636f6e2e636f6d2f69636f6e732f7376672f3931392f3931393834372e737667"
+        />
+      </a>
     </div>
-    <a
-      className="home__gh-link"
-      href="https://github.com/2AMDevs/invoicify-app"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <img
-        alt="github icon"
-        className="home__gh-link__img"
-        src="https://camo.githubusercontent.com/351dcb42fb2be38ad786b8e29aa8b6b9dc9c2c8905aa451a1074f1ca5a76ce64/68747470733a2f2f696d6167652e666c617469636f6e2e636f6d2f69636f6e732f7376672f3931392f3931393834372e737667"
-      />
-    </a>
-  </div>
-)
+  )
+}
 
 export default Home
