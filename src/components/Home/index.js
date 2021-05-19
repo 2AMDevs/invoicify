09973e8be5ffd20284e2482cbd92cc0e6ee97b86
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2021-05-19 14:37:44 +0530

    Redesign Header (#11)
    * remove bg
    
    * add images to landing page
    
    * show horizontal imgs
    
    * add github link to header
    
    * responsive nav
    
    * skey gradient
    
    * style github button
    
    * allow scroll
    
    * style gradient
    
    * letter spacing download button
    
    * reduce radius
    
    * add logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,68 +1,72 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import { Button } from '../Elements'
 import HomePerks from './HomePerks'
 
 import './index.scss'
 
-const Home = () => (
+// const screenshots = [
+//   'lock', 'personalize', 'preview',
+//   'print', 'settings', 'update',
+// ]
+
+const Home = (props) => (
   <div className="home">
-    <img
-      className="home__bg-img"
-      src="img/home-bg-swirl.jpg"
-      alt="swirrrrrrrrrrrrl"
-    />
     <div className="home__content">
       <div className="home__content__nav">
         <div className="home__content__nav__header">
-          <h1 className="home__content__nav__header__brand">Invoicify</h1>
-          <Link to="/download">
-            <Button
-              secondary
-              className="home__content__nav__help-btn"
-            >
-              Why Invoicify?
-            </Button>
-          </Link>
+          <div className="home__content__nav__header__brand">
+            <img
+              className="home__content__nav__header__brand__logo"
+              src="/favicon.png"
+              alt="Invoicify brand logo"
+            />
+            Invoicify
+          </div>
+          <a
+            className="home__content__nav__header__github"
+            href="https://github.com/2AMDevs/invoicify-app"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              alt="github icon"
+              className="home__content__nav__header__github__img"
+              src="https://camo.githubusercontent.com/351dcb42fb2be38ad786b8e29aa8b6b9dc9c2c8905aa451a1074f1ca5a76ce64/68747470733a2f2f696d6167652e666c617469636f6e2e636f6d2f69636f6e732f7376672f3931392f3931393834372e737667"
+            />
+          </a>
         </div>
       </div>
+      <div className="home__content__grad" />
       <div className="home__content__wrapper">
-        <div className="home__content__main">
-          <div className="home__content__main__text">
-            <h1 className="home__content__main__text__title">Extremely customizable</h1>
-            <h1 className="home__content__main__text__sub-title">& reliable Invoice software</h1>
-            <h1 className="home__content__main__text__sub-title--small">A complete invoice solution for businesses</h1>
+        <div className="home__content__wrapper__hero">
+          <div className="home__content__wrapper__hero__text">
+            <h1>One Stop Solution for all your invoice needs</h1>
+            <h2>Highly configurable</h2>
+            <Button
+              secondary
+              className="home__content__wrapper__hero__download-btn"
+              onClick={() => props.history.push('/download')}
+            >
+              Download Now
+            </Button>
           </div>
-          <div className="home__content__main__download-section">
-            <Link to="/download">
-              <Button
-                secondary
-                className="home__content__main__download-section__dwnld-btn"
-              >
-                Download
-              </Button>
-            </Link>
+          <div className="home__content__wrapper__hero__perks">
+            <HomePerks />
           </div>
         </div>
 
-        <div className="home__content__perks">
-          <HomePerks />
-        </div>
+        {/* <div className="home__content__main">
+          {screenshots.map((ss) => (
+            <img
+              className="home__content__main__img"
+              alt={ss}
+              src={`img/ss/${ss}.jpg`}
+            />
+          ))}
+        </div> */}
       </div>
     </div>
-    <a
-      className="home__gh-link"
-      href="https://github.com/2AMDevs/invoicify-app"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <img
-        alt="github icon"
-        className="home__gh-link__img"
-        src="https://camo.githubusercontent.com/351dcb42fb2be38ad786b8e29aa8b6b9dc9c2c8905aa451a1074f1ca5a76ce64/68747470733a2f2f696d6167652e666c617469636f6e2e636f6d2f69636f6e732f7376672f3931392f3931393834372e737667"
-      />
-    </a>
   </div>
 )
 
