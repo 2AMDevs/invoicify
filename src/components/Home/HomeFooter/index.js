f3c9672f7387380591b17bc2c08611346c8ae732
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-05-22 15:24:36 +0530

    move flag to company name

--- src/components/Home/HomeFooter/index.js
+++ src/components/Home/HomeFooter/index.js
@@ -4,14 +4,14 @@ import './index.scss'
 
 const HomeFooter = () => (
   <div className="home-footer">
-    <div className="home-footer__left">
+    <div className="home-footer__left" />
+    <div className="home-footer__right">
+
       <div className="home-footer__flag">
         <div className="home-footer__flag__saffron" />
         <div className="home-footer__flag__white" />
         <div className="home-footer__flag__green" />
       </div>
-    </div>
-    <div className="home-footer__right">
       <div className="home-footer__right__company">
         © 2AM Devs
       </div>
