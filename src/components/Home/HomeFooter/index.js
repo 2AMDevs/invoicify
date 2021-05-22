87d7023df392781479489e991cc66d7455981637
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-05-22 15:37:37 +0530

    add links

--- src/components/Home/HomeFooter/index.js
+++ src/components/Home/HomeFooter/index.js
@@ -4,7 +4,27 @@ import './index.scss'
 
 const HomeFooter = () => (
   <div className="home-footer">
-    <div className="home-footer__left" />
+    <div className="home-footer__left">
+      <div className="home-footer__left__links">
+        <p>Links</p>
+        <a
+          className="home-footer__left__links__link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/2AMDevs/invoicify-app"
+        >
+          GitHub
+        </a>
+        <a
+          className="home-footer__left__links__link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://discord.gg/UgvYpNrHa6"
+        >
+          Join Chat on Discord
+        </a>
+      </div>
+    </div>
     <div className="home-footer__right">
 
       <div className="home-footer__flag">
